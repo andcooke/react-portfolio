1c4a1f5c8c369840e54498745ca8e05e6ab13711
A <andcooke@gmail.com>
2022-08-24 20:47:22 -0500

    some initial files created

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+// import Footer from './Footer';
+// import About from './pages/About';
+// import Contact from './pages/Contact';
+// import Portfolio from './pages/Portfolio';
+// import Resume from './pages/Resume';
+
+export default function PortfolioContainer () {
+
+
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}

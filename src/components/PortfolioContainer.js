e16b6453776e04a16c9b6fa151d80241b07474d1
A <andcooke@gmail.com>
2022-08-25 19:06:54 -0500

    footer component rendering

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import Header from './Header';
-// import Footer from './Footer';
+// import Header from './Header';
+import Footer from './Footer';
 // import About from './pages/About';
 // import Contact from './pages/Contact';
 // import Portfolio from './pages/Portfolio';
 // import Resume from './pages/Resume';
+import '../styles/styles.css'
 
 export default function PortfolioContainer () {
 
 
   return (
     <div>
-      <Header />
+          
+      <Footer />
     </div>
   )
 }

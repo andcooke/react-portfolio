1bc7911d600038b1c140d2c90e683bf30addb1f6
A <andcooke@gmail.com>
2022-08-26 20:44:58 -0500

    links clicking and changing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,48 @@
 import React from 'react';
 
-function Header(currentPage, handlePageChange) {
-
-
+function Header({ currentPage, handlePageChange }) {
 
   return (
-    <div>
+    <div className="header">
+
+      <div className="header-initials">
+        <h1 className="initials">A. C.</h1>
+      </div>
+
+      <div className="navbar">
+        <ul className="link-container">
+          <li>
+            <a 
+              href="#about" 
+              onClick={() => handlePageChange('About')}
+              className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
+                about
+            </a>
+          </li>
+          <li>
+            <a 
+              href="#portfolio"
+              onClick={() => handlePageChange('Portfolio')}
+              className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
+                portfolio
+            </a>
+          </li>
+          <li>
+            <a href="#contact"
+              onClick={() => handlePageChange('Contact')}
+              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+                contact
+            </a>
+          </li>
+          <li>
+            <a href="#résumé"
+              onClick={() => handlePageChange('Resume')}
+              className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>
+                résumé
+            </a>
+          </li>
+        </ul>
+      </div>
 
     </div>
   )
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,10 +1,10 @@
-import React from 'react';
-// import Header from './Header';
+import React, { useState } from 'react';
+import Header from './Header';
 import Footer from './Footer';
-// import About from './pages/About';
-// import Contact from './pages/Contact';
-// import Portfolio from './pages/Portfolio';
-// import Resume from './pages/Resume';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 import '../styles/styles.css'
 
 export default function PortfolioContainer () {

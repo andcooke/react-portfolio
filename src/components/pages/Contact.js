8b6a359b414aa87303d18642bd6d136f2d8898ca
A <andcooke@gmail.com>
2022-08-27 20:42:38 -0500

    contact form made with simple css

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,23 +2,21 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+    <div className="contact-container">
+      <div className="user-info">
+        <input className="name-input" id="contact-name" placeholder='name'></input>
+        <input className="email-input" id="contact-email" placeholder='email'></input>
+      </div>
+      <div className="message-content">
+        <input className="subject-input" id="contact-subject" placeholder='subject'></input>
+        <textarea className="message-input" id="contact-message" placeholder='message'></textarea>
+      </div>
+      <div className="send-button">
+        <button>Send</button>
+      </div>
     </div>
   );
 }
+
+
+          

7af640f1c3b3752277ebb2d0c5513ab83a40788b
A <andcooke@gmail.com>
2022-08-29 17:14:14 -0500

    emails sending git push

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 //look at React Forms again in the coursework
 
 export default function Contact() {
 
+  const [formState, setFormState] = useState({
+    user_name: "",
+    user_email: "",
+    subject: "",
+    message: "",
+  });
+
+  const [hiddenState, setHiddenState] = useState(true);
+
+  const { user_name, user_email, subject, message } = formState;
+
+  function handleChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_c01fp3m', 'template_0759zjm', form.current, 'HLScYYxkOCDwDU0SN')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+    setFormState({user_name: "", user_email: "", subject: "", message: ""})
+    setHiddenState(false);
+  }
+
   return (
-  <div className="contact-form">
-    <input type="hidden" name="contact_number"/>
+  <form ref={form} onSubmit={sendEmail} className="contact-form">
     <div className="user-info">
-      <input type="text" name="user_name" className="name-input" id="contact-name" placeholder='name'></input>
-      <input type="email" name="user_email" className="email-input" id="contact-email" placeholder='email'></input>
+      <input type="text" value={user_name} name="user_name" className="name-input" id="contact-name" placeholder='name' onChange={handleChange}></input>
+      <input type="email" value={user_email} name="user_email" className="email-input" id="contact-email" placeholder='email' onChange={handleChange}></input>
     </div>
     <div className="message-content">
-      <input name="subject" className="subject-input" id="contact-subject" placeholder='subject'></input>
-      <textarea name="message" className="message-input" id="contact-message" placeholder='message'></textarea>
+      <input name="subject" value={subject} className="subject-input" id="contact-subject" placeholder='subject' onChange={handleChange}></input>
+      <textarea name="message" value={message} className="message-input" id="contact-message" placeholder='message' onChange={handleChange}></textarea>
     </div>
     <div className="send-button">
       <input type="submit" value="send" id="send-email"/>
     </div>
-  </div>
+    <div className= { hiddenState ? "hidden-toast" : ""}>
+      Sent
+    </div>
+  </form>
   );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,28 +5,41 @@ export default function Portfolio() {
   
   let projects = [
     {
-      name: "Tech Blog"
+      name: "Tech Blog",
+      link: "https://github.com/andcooke/tech-blog",
+      description: 3,
     },
     {
-      name: "The Shadow Tome" 
+      name: "The Shadow Tome",
+      link: "https://github.com/andcooke/the-shadow-tome",
+      description: 3,
     },
     {
-      name: "Work Day Scheduler"
+      name: "Work Day Scheduler",
+      link: "https://github.com/andcooke/work-day-scheduler",
+      description: 3,
     },
     {
-      name: "Git Me There"
+      name: "Git Me There",
+      link: "https://github.com/andcooke/git-me-there",
+      description: 3,
     },
     {
-      name: "Weather Dashboard"
+      name: "Weather Dashboard",
+      link: "https://github.com/andcooke/weather-dashboard",
+      description: 3,
     },
     {
-      name: "Employee Tracker"
+      name: "Employee Tracker",
+      link: "https://github.com/andcooke/employee-tracker",
+      description: 3,
     }
   ]
 
   return (projects.map((item) => (
-      <div>
-       <p>{item.name}</p>
+      <div className="portfolio-container">
+        <p>{item.name}</p>
+        <a href={item.link}>test</a>
       </div> 
     ))
   )

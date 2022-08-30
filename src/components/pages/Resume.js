7b147a838bf7a620dba61fa9d5e389940ce15baf
A <andcooke@gmail.com>
2022-08-30 17:58:05 -0500

    starting resume work

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,22 +1,55 @@
 import React from 'react';
+import resume from '../../images/resume.pdf';
+
 
 export default function Resume() {
+
   return (
-    <div>
-      <h1>Resume</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+    <div className="resume-container">
+      <div className="engineer-title">
+        <h2>SOFTWARE ENGINEER</h2>
+      </div>
+
+      <div className="skills-container">
+        <div className="resume-header">
+          <h2>skills</h2>
+        </div>
+      </div>
+
+      <div className="experience-container">
+        <div className="resume-header">
+          <h2>experience</h2>
+        </div>
+        <div className="experience">
+        <p>
+        Austin Independent School District - Art Teacher -- McCallum Fine Arts Academy	2014-2022
+          Wrote and implemented curriculum for 5 different courses and 180+ students each year
+          Started and grew the Digital Arts program, managing equipment, software, and curriculum
+          Oversaw participation in regional and national competitions, garnering top awards annually
+          Adapted all curriculum and instruction for online-learning during the covid-19 pandemic
+        </p>
+        </div>
+      </div>
+
+      <div className="education-container">
+        <div className="resume-header">
+          <h2>education</h2>
+        </div>
+        <div className="education">
+          <p>
+          UT Austin Boot Camp - Full-Stack Coding Immersive Program	2022	
+
+          Sam Houston State University - Bachelor of Fine Arts, Photography, Cum Laude
+          </p>
+        </div>
+
+        <div>
+          <a href={resume} download className="download-button">download</a>
+        </div>
+      </div>
     </div>
   );
 }
+
+
+{/* <a href={resume} download> download</a> */}

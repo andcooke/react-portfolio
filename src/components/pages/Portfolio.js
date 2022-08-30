9b2a8798edf2ce49fa7770f4e080d48164385608
A <andcooke@gmail.com>
2022-08-29 22:02:53 -0500

    simple project cards made

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -7,40 +7,38 @@ export default function Portfolio() {
     {
       name: "Tech Blog",
       link: "https://github.com/andcooke/tech-blog",
-      description: 3,
     },
     {
       name: "The Shadow Tome",
       link: "https://github.com/andcooke/the-shadow-tome",
-      description: 3,
     },
     {
       name: "Work Day Scheduler",
       link: "https://github.com/andcooke/work-day-scheduler",
-      description: 3,
     },
     {
       name: "Git Me There",
       link: "https://github.com/andcooke/git-me-there",
-      description: 3,
     },
     {
       name: "Weather Dashboard",
       link: "https://github.com/andcooke/weather-dashboard",
-      description: 3,
     },
     {
       name: "Employee Tracker",
       link: "https://github.com/andcooke/employee-tracker",
-      description: 3,
     }
   ]
 
-  return (projects.map((item) => (
+  return (
       <div className="portfolio-container">
-        <p>{item.name}</p>
-        <a href={item.link}>test</a>
+        {projects.map((item) => (
+          <div className="project">
+            <a href={item.link}>
+              <p>{item.name}</p>
+            </a>
+          </div>
+        ))}
       </div> 
-    ))
   )
 }

7247695d87998369c4a49fa1df880548c3ff3d5e
A <andcooke@gmail.com>
2022-10-12 12:18:05 -0500

    adding summaries to projects

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -6,31 +6,37 @@ export default function Portfolio() {
   let projects = [
     {
       name: "Baes",
+      summary: "baes",
       deployed: "https://loversnotfighters.herokuapp.com/",
       github: "https://github.com/andcooke/baes",
     },
     {
       name: "The Shadow Tome",
+      summary: "shadows",
       deployed: "https://the-shadow-tome.herokuapp.com/",
       github: "https://github.com/andcooke/the-shadow-tome",
     },
     {
       name: "Git Me There",
+      summary: "git",
       deployed: "https://andcooke.github.io/git-me-there/",
       github: "https://github.com/andcooke/git-me-there",
     },
     {
       name: "Tech Blog",
+      summary: "tech",
       deployed: "http://tech-tech-blog-blog.herokuapp.com/",
       github: "https://github.com/andcooke/tech-blog",
     },
     {
       name: "Work Day Scheduler",
+      summary: "scheduler",
       deployed: "https://andcooke.github.io/work-day-scheduler/",
       github: "https://github.com/andcooke/work-day-scheduler",
     },
     {
       name: "Weather Dashboard",
+      summary: "weather",
       deployed: "https://andcooke.github.io/weather-dashboard/",
       github: "https://github.com/andcooke/weather-dashboard",
     },
@@ -42,11 +48,10 @@ export default function Portfolio() {
           <a className="project-effect">
             <div className="project" key={i}>
               <p className="project-title">{item.name}</p>
-              <div className="project-links">
+              <div className="project-info">
+                <p>{item.summary}</p>
                 <div className="links">
                   <a href={item.github}>github</a>
-                </div>
-                <div className="links">
                   <a href={item.deployed}>deployed</a>
                 </div>
               </div>

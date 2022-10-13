26e61b37ca407b898551597c087f8432f2fcea4a
A <andcooke@gmail.com>
2022-10-13 10:17:51 -0500

    working on portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -6,37 +6,37 @@ export default function Portfolio() {
   let projects = [
     {
       name: "Baes",
-      summary: "baes",
+      summary: "Full stack relationship app with social networking capabilities",
       deployed: "https://loversnotfighters.herokuapp.com/",
       github: "https://github.com/andcooke/baes",
     },
     {
       name: "The Shadow Tome",
-      summary: "shadows",
+      summary: "Full stack two-player turn-based fighting game",
       deployed: "https://the-shadow-tome.herokuapp.com/",
       github: "https://github.com/andcooke/the-shadow-tome",
     },
     {
       name: "Git Me There",
-      summary: "git",
+      summary: "Front-end travel site with third party APIs",
       deployed: "https://andcooke.github.io/git-me-there/",
       github: "https://github.com/andcooke/git-me-there",
     },
     {
       name: "Tech Blog",
-      summary: "tech",
+      summary: "Full stack blog site with social networking capabilities",
       deployed: "http://tech-tech-blog-blog.herokuapp.com/",
       github: "https://github.com/andcooke/tech-blog",
     },
     {
       name: "Work Day Scheduler",
-      summary: "scheduler",
+      summary: "Front-end site to ",
       deployed: "https://andcooke.github.io/work-day-scheduler/",
       github: "https://github.com/andcooke/work-day-scheduler",
     },
     {
       name: "Weather Dashboard",
-      summary: "weather",
+      summary: "Front-end weather site with third party APIs",
       deployed: "https://andcooke.github.io/weather-dashboard/",
       github: "https://github.com/andcooke/weather-dashboard",
     },
